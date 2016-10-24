fc03d3d455c10e7802591f4f34a0a1c84dba6934
Valera Kabanec <kabanecvalera@gmail.com>
2016-10-24 22:45:21 +0300

    commit 1

---
+++ js/script.js
@@ -0,0 +1,15 @@
+(function() {
+    
+    var doc = document;
+    var head = doc.createElement("h1");
+    head.innerHTML = 'Заголовок';
+    doc.body.appendChild( head );
+ 
+})();
+
+/*var myheader = document.createElement('span');
+            myheader.innerHTML = header;
+            myheader.style.margin = '0 550px';
+            myheader.style.font = '16px Arial';
+            document.body.appendChild(myheader);
+  */
